588343f8782f7de4d216667e1795faca6ad98820
lucas271102 <lucas001spe@gmail.com>
2023-09-25 10:33:03 -0300

    setupServer

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require('dotenv').config()
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,6 +21,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+const port = 8000
+app.listen(port, ()=>{
+  console.log(`Server is listening on port ${port}`)
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
