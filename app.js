769e263e8deb8f48d95d536cba79e8b568befd8e
lucas271102 <lucas001spe@gmail.com>
2023-09-25 17:48:24 -0300

    Conexión con PostgreSQL

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var logger = require('morgan');
 require('dotenv').config()
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const db = require('./db')
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -26,16 +26,72 @@ const port = process.env.PORT || 8080
 app.listen(port, ()=>{
   console.log(`Server is listening on port ${port}`)
 })
+app.use((req, res, next)=>{
+  console.log('Middleware')
+  next()
+})
 
 
-app.get('/getRestaurants', (req, res)=>{
+//Get all restaurants
+app.get('/api/v1/restaurants',async  (req, res)=>{
+   const results = await db.query('select * from restaurants')
+  console.log(results)
   res.json({
     success: true,
     date: new Date,
-    restaurant:'McDonald´s'
+    data:{
+
+      restaurant:['McDonalds', 'Five Guys']
+    }
   })
   console.log('get all restaurants')
 })
+
+
+//Get one restaurant
+app.get('/api/v1/restaurants/:id', (req, res)=>{
+  console.log(req.params)
+  res.status(200).json({
+    status:'success',
+    data:{
+      restaurant:'mcdonalds'
+    }
+  })
+})
+
+//Create a restaurant
+app.post('/api/v1/restaurants',(req, res)=>{
+  console.log(req.body)
+  res.status(201).json({
+    status:'success',
+    data:{
+      restaurant:'mcdonalds'
+    }
+  })
+})
+
+//Update restaurants
+
+app.put('/api/v1/restaurants/:id',(req, res)=>{
+console.log(req.params.id);
+console.log(req.body)
+res.status(200).json({
+  status:'success',
+  data:{
+    restaurant:'mcdonalds'
+  }
+})
+})
+
+//Delete restaurants
+app.delete('/api/v1/restaurants/:id', (req, res)=>{
+  res.status(204).json({
+    status:'Successfully deleted'
+  }
+    )
+})
+
+
 console.log('test')
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
