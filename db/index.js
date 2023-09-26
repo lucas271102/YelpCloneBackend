d8d841d5467dbe452335725e2b777dc8a03eddcb
lucas271102 <lucas001spe@gmail.com>
2023-09-26 17:38:55 -0300

    Falta finalizar rutas CRUD

--- db/index.js
+++ db/index.js
@@ -1,17 +1,13 @@
-const {Pool}= require('pg')
- 
+const { Pool } = require("pg");
+
 const pool = new Pool({
-
-
-    user: process.env.PGUSER,
+  user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
+});
 
-}
-)
- 
-module.exports={
-  query:(text, params)=>pool.query(text, params),
-}
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
